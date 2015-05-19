7880ab6b3401e88a04d136ddcb0a8f15b1da83cf
Gwenael617 <gwenael_gouerou@yahoo.fr>
2015-05-19 07:56:39 +0300

    update Swed

--- Vilnius/swedbank2.R
+++ Vilnius/swedbank2.R
@@ -7,7 +7,7 @@ cat("to start the computation, write computeStatement()")
 
 computeStatement <- function() {
 
-  dateOfFile <- "2015-04-30"
+  dateOfFile <- "2015-05-19"
   file <- paste("C:/Users/user/Documents/_Documents importants/Banques/Swedbank/Swedbank_statement_", 
                 dateOfFile, ".csv", sep = "")
   
